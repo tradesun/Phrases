70dff4bb932098ecf6c0cd8d5aa1e0d8e85932dd
tradesun <tradesun@gmail.com>
2017-09-04 18:19:30 +0530

    Add files via upload

---
+++ download_images.php
@@ -0,0 +1,54 @@
+<?php
+
+
+$names_r = "Ramaria flava Schaeff,Macrolepiota procera,Armillariella mellea,Pleurotus ostreatus,Lactarius deliciosus,Russula rosea Pers,Suillus granulatus,Suillus luteus,Cantharellus cibarius";
+
+$images_r = "http://www.mycobg.com/sites/default/files/styles/gallery_up/public/photos/01_149242798.jpg?itok=phW49uii,http://www.gobice.com/pic/Ramaria_flava_-_rumena_griva_01.jpg,http://www.gobice.com/pic/Ramaria_flava_-_rumena_griva_03.jpg,https://www.nahuby.sk/images/fotosutaz/2004/ladislav_pomsar2004_5339.jpg,http://www.mycobg.com/sites/default/files/styles/gallery_up/public/photos/02_149242799.jpg?itok=-dwncjj9,https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Ramaria_flava_a1_%2810%29.JPG/240px-Ramaria_flava_a1_%2810%29.JPG@@http://www.gobice.com/pic/macrolepiota_procera_-_orjaski_deznik_03.jpg,http://mushroomhobby.com/Gallery/Lepiota/Macrolepiota%20procera/Macrolepiota%20procera%201.jpg,http://www.first-nature.com/fungi/images/agaricaceae/macrolepiota-procera2.jpg,http://www.discoverlife.org/IM/I_MWS/1149/640/Macrolepiota_procera_var._procera,I_MWS114944.jpg,http://2.bp.blogspot.com/-AtrL8ztxDgI/VLbWdvrboiI/AAAAAAAAUbs/duR_pEx61SU/s1600/Macrolepiota%2Bprocera_himenio.jpg,https://upload.wikimedia.org/wikipedia/commons/c/cf/Parasol-Macrolepiota-procera.jpg@@http://www.gobice.com/pic/armillaria_mellea_-_sivorumena_mraznica_01.jpg,https://upload.wikimedia.org/wikipedia/commons/8/84/Armillaria_mellea%2C_Honey_Fungus%2C_UK_1.jpg,http://www.gobice.com/pic/armillaria_mellea_-_sivorumena_mraznica_04.jpg,https://images.nbnatlas.org/image/proxyImageThumbnail?imageId=4d84d4ee-007a-4939-aafa-757d4faf5f48,https://i.pinimg.com/originals/1c/f5/d6/1cf5d67bfac52bc51bef7e69ffcd3153.jpg,https://farm9.static.flickr.com/8461/8064093886_570a030dbe_b.jpg,https://images.nbnatlas.org/image/proxyImageThumbnailLarge?imageId=12ff2595-1ba6-4f44-9712-5c5fefcb070c@@http://www.gobice.com/pic/Pleurotus_ostreatus_-_Bukov_ostrigar_03.jpg,http://www.gobice.com/pic/Pleurotus_ostreatus_-_Bukov_ostrigar_05.jpg,https://www.healthbenefitstimes.com/9/gallery/oyster-mushroom/Oyster-mushroom-2.jpg,https://www.healthbenefitstimes.com/9/uploads/2015/06/Pleurotus-ostreatus.png,http://www.mykoweb.com/CAF/photos/large/Pleurotus_ostreatus(mgw-03).jpg,http://www.mykoweb.com/CAF/photos/large/Pleurotus_ostreatus(fs-04).jpg,http://ncrfungi.uark.edu/species/88_pleurotusOstreatus/sm00PK.jpg@@http://www.first-nature.com/fungi/images/russulaceae/lactarius-deliciosus1.jpg,http://www.first-nature.com/fungi/images/russulaceae/lactarius-deliciosus7.jpg,http://c8.alamy.com/comp/EYYH5M/saffron-milk-cap-saffron-milkcap-red-pine-mushroom-lactarius-deliciosus-EYYH5M.jpg,http://4.bp.blogspot.com/_zKDjcLXZBTI/RmcvD6FrKgI/AAAAAAAAApw/bOQn-S2EQl8/s400/BlogF015b.jpg,http://media.istockphoto.com/illustrations/mushrooms-and-fungi-lactarius-deliciosus-illustration-id466361758,http://www.gobice.com/pic/lactarius_deliciosus_-_uzitna_sirovka_03.jpg,http://www.gobice.com/pic/lactarius_deliciosus_-_uzitna_sirovka_0003.jpg@@http://www.gobice.com/pic/russula_rosea_07.jpg,http://www.gobice.com/pic/russula_rosea_09.jpg,http://www.first-nature.com/fungi/images/russulaceae/russula-rosea2.jpg,http://www.gobice.com/pic/russula_rosea_11.jpg,http://c8.alamy.com/comp/EGX949/crab-brittlegill-shrimp-mushroom-shrimp-russula-russula-xerampelina-EGX949.jpg,http://www.first-nature.com/fungi/images/russulaceae/russula-rosea5.jpg,http://mushrooms.pidisoft.cz/produkty/107-holubinka-ruzova/108-9106-11.jpg,https://i.pinimg.com/236x/d8/7e/fa/d87efac8a9668d4b61e2da0240f0a019--rosacea-fungi.jpg@@http://www.asergeev.com/pictures/archives/2016/1857/jpeg/15b.jpg,https://upload.wikimedia.org/wikipedia/commons/1/1c/Suillus_granulatus_1.jpg,http://2.bp.blogspot.com/-brZFW6bFzKo/VkIkOgtM0RI/AAAAAAAAKXw/THNzPDIWcz4/s1600/Suillus%2Bgranulatus%2B%2528L.%2BFr.%2529%2BKuntze%2BIMG_9877%2B%2528Copy%2529.jpg,https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Suillus_luteus_475376.jpg/1200px-Suillus_luteus_475376.jpg,http://www.first-nature.com/fungi/images/suillaceae/suillus-granulatus1.jpg,https://farm9.static.flickr.com/8403/29066980873_a49459c96e_b.jpg,https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Suillus_granulatus.jpg/426px-Suillus_granulatus.jpg,https://d1k5w7mbrh6vq5.cloudfront.net/images/cache/5a/7e/5c/5a7e5c7ee3959ca8b1ea40d3dcd80961.jpg@@http://www.first-nature.com/fungi/images/suillaceae/suillus-luteus1.jpg,http://www.gobice.com/pic/suillus_luteus_09.jpg,http://www.discoverlife.org/IM/I_MWS/0303/320/Suillus_luteus,I_MWS30325.jpg,http://www.gobice.com/pic/suillus_luteus_06.jpg,http://www.first-nature.com/fungi/images/suillaceae/suillus-luteus3.jpg,http://www.discoverlife.org/IM/I_MWS/0303/640/Suillus_luteus,I_MWS30327.jpg,https://i1.wp.com/www.fungusfactfriday.com/wp-content/uploads/2016/09/Suillus-luteus-slimy.jpg@@http://www.fichasmicologicas.com/typo3temp/pics/6211af8980.jpg,http://www.mushroomexpert.com/images/kuo3/cantharellus_cf_cibarius_02.jpg,http://www.first-nature.com/fungi/images/cantharellales/cantharellus-cibarius1.jpg,http://www.indianamushrooms.com/images/cantharellus_cibarius_1.JPG,http://www.mushroomexpert.com/images/nadon/nadon_cantharellus_cibarius.jpg,https://upload.wikimedia.org/wikipedia/commons/8/86/Cantharellus_cibarius.JPG";
+
+$names = explode(',',$names_r);
+
+
+$images = explode('@@',$images_r);
+
+//for ($x=0;$x<1;$x++){
+for ($x=0;$x<count($names);$x++){
+
+	mkdir($names[$x]);
+	$imag = explode(',',$images[$x]);
+	for ($y=0;$y<count($imag);$y++){
+		
+		$img= str_replace("\r","",$imag[$y]);
+		$img= str_replace("\n","",$img);
+		//echo $img  . '<br>';
+		gurl($img,$names[$x] . '/' .$y);
+		
+	}
+	
+	//echo 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
+}
+
+
+//gurl('https://upload.wikimedia.org/wikipedia/commons/6/6a/Cowberry.jpg','1');
+
+function gurl($url,$file){
+$ch = curl_init($url);
+$fp = fopen($file . ".jpg", "w");
+
+curl_setopt($ch, CURLOPT_FILE, $fp);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	
+curl_exec($ch);
+	
+	if($errno = curl_errno($ch)) {
+    $error_message = curl_strerror($errno);
+    echo "cURL error ({$errno}):\n {$error_message}";
+}
+	
+curl_close($ch);
+fclose($fp);
+	
+}
+
+
---
+++ index1.php
@@ -0,0 +1,39 @@
+
+<html><header><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<style>
+
+img {
+    width: 33%;
+}
+	
+	h2 {
+    font-size: 25px;
+}
+</style>
+</header><body>
+	
+
+<?php
+
+$plants = "Sambucus ebulus,Sambucus nigra,Viburnum lantana,Viburnum opulus,Amaranthus palmeri,Beta vulgaris,Allium sativum,Allium victorialis,Carum carvi,Tagetes patula,Berberis vulgaris,Corylus avellana,Corylus pontica,Brassica oleracea,Campanula lactiflora,Cucurbita pepo,Diospyros lotus,Vaccinium myrtillus,Vaccinium vitis-idaea,Trigonella caerulea,Castanea sativa Mill,Fagus orientalis Lipsky,Quercus iberica,Ribes grossularia,Ribes nigrum,Ribes rubrum,Juglans regia,Mentha x piperita,Morus alba,Polygonum alpinum,Polygonum aviculare,Rumex acetosa,Crataegus curvisepala Lindm,Crataegus pentagyna Waldst,Fragaria vesca,Malus orientalis Uglizk,Mespilus germanica,Prunus avium,Prunus x domestica,Pyrus caucasica Fed,Pyrus communis,Rubus idaeus,Sorbus caucasigena,Sorbus torminalis,Capsicum annuum,Staphylea colchica,Agaricus arvensis Schaeff,Agaricus campestris,Amanita caesarea,Ramaria flava Schaeff,Macrolepiota procera,Armillariella mellea,Pleurotus ostreatus,Lactarius deliciosus,Russula rosea Pers,Suillus granulatus,Suillus luteus,Cantharellus cibarius";
+$ri = explode(',',$plants);
+
+foreach($ri as $row){
+	echo "<h2><a href='sites/{$row}.html' > $row </a> </h2>";
+	
+	$dir = new DirectoryIterator("images/" . $row);
+	foreach ($dir as $fileinfo) {
+    if (!$fileinfo->isDot()) {
+		$img = 'images/' . $row . '/' . $fileinfo->getFilename();
+		echo '<a href="' . $img . '"><img src="' . $img . '"/></a>';
+        
+    }
+}
+}
+?>
+
+
+</body>	
+	
+
+</html>
